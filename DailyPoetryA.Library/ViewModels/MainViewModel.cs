e53470e4eb92b2de341bd85b685d5b3d5dc385c0
Zhang, Yin <zhangyin@mail.neu.edu.cn>
2024-02-28 23:28:05 +0800

    NA

--- DailyPoetryA.Library/ViewModels/MainViewModel.cs
+++ DailyPoetryA.Library/ViewModels/MainViewModel.cs
@@ -1,3 +1,33 @@
+using System.Windows.Input;
+using CommunityToolkit.Mvvm.Input;
+
 namespace DailyPoetryA.Library.ViewModels;
 
-public class MainViewModel : ViewModelBase { }
+public class MainViewModel : ViewModelBase {
+    public MainViewModel() {
+        OpenPaneCommand = new RelayCommand(OpenNavigationPane);
+        ClosePaneCommand = new RelayCommand(CloseNavigationPane);
+    }
+
+    private string _title = "DailyPoetryA";
+
+    public string Title {
+        get => _title;
+        set => SetProperty(ref _title, value);
+    }
+
+    private bool _isPaneOpen;
+
+    public bool IsPaneOpen {
+        get => _isPaneOpen;
+        set => SetProperty(ref _isPaneOpen, value);
+    }
+
+    public ICommand OpenPaneCommand { get; }
+
+    public void OpenNavigationPane() => IsPaneOpen = true;
+
+    public ICommand ClosePaneCommand { get; }
+
+    public void CloseNavigationPane() => IsPaneOpen = false;
+}
--- DailyPoetryA/Program.cs
+++ DailyPoetryA/Program.cs
@@ -1,5 +1,7 @@
 ﻿using Avalonia;
 using System;
+using Projektanker.Icons.Avalonia;
+using Projektanker.Icons.Avalonia.FontAwesome;
 
 namespace DailyPoetryA;
 
@@ -8,9 +10,14 @@ sealed class Program {
     // SynchronizationContext-reliant code before AppMain is called: things aren't initialized
     // yet and stuff might break.
     [STAThread]
-    public static void Main(string[] args) => BuildAvaloniaApp().StartWithClassicDesktopLifetime(args);
+    public static void Main(string[] args) =>
+        BuildAvaloniaApp().StartWithClassicDesktopLifetime(args);
 
     // Avalonia configuration, don't remove; also used by visual designer.
-    public static AppBuilder BuildAvaloniaApp() =>
-        AppBuilder.Configure<App>().UsePlatformDetect().WithInterFont().LogToTrace();
+    public static AppBuilder BuildAvaloniaApp() {
+        IconProvider.Current.Register<FontAwesomeIconProvider>();
+
+        return AppBuilder.Configure<App>().UsePlatformDetect().WithInterFont()
+            .LogToTrace();
+    }
 }
